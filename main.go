913a2de9b2273fb6c9b628b56a5db6887b30ea1f
anxielray <anxielworld@gmail.com>
2024-07-14 03:12:59 +0200

    Updated main and implemented most things

--- main.go
+++ main.go
@@ -3,33 +3,26 @@ package main
 import (
 	"fmt"
 	"os"
+
+	tetro "tetraGame/tetromio"
 )
 
 func main() {
-	//check cmd arguments
-	if len(os.Args) != 2 {
-		fmt.Println("ERROR!")
-		os.Exit(0)
+	args := os.Args[1]
+	if args != "" {
+		file, err := os.Open(args)
+		if err != nil {
+			fmt.Println("ERROR")
+			os.Exit(1)
+		}
+		defer func() {
+			if err = file.Close(); err != nil {
+				fmt.Println("ERROR")
+				os.Exit(1)
+			}
+		}()
+		var myArray = tetro.ReadInput(file)
+		tetro.Solve(myArray)
+		tetro.PrintSol()
 	}
-	//read test file
-	fileName := os.Args[1]
-	file, err := os.Open(fileName)
-	if err != nil {
-		fmt.Println("ERROR")
-		os.Exit(0)
-	}
-	defer file.Close()
-	/*validation of tetrominoes
-	* 1. check if all tetrominoes are unique
-	* 3. check if all tetrominoes are correct
-	  - It should have exactly 4 rows.
-	  - Each row should have exactly 4 characters ('#' or .).
-	  - There should be exactly 4 '#' characters in total.
-	  - Ensure that each tetromino has exactly 6 sides of '#' cells touching.
-	*/
-
-	//board calculation
-
-	//backtracking algorithm
-
 }
