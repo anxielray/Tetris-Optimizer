6833ecd889077bde524e637c9e1c9aa1c9d953bc
anxielray <anxielworld@gmail.com>
2024-07-17 20:24:52 +0200

    Add main

--- main.go
+++ main.go
@@ -3,17 +3,19 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	tetro "tetraGame/tetromio"
 )
 
 func main() {
+	startTime := time.Now()
 	args := os.Args[1]
 	if args != "" {
 		file, err := os.Open(args)
 		if err != nil {
 			fmt.Println("ERROR")
-			os.Exit(1)
+			os.Exit(0)
 		}
 		defer func() {
 			if err = file.Close(); err != nil {
@@ -24,5 +26,8 @@ func main() {
 		var myArray = tetro.ReadInput(file)
 		tetro.Solve(myArray)
 		tetro.PrintSol()
+		end := time.Now()
+		elapsed := end.Sub(startTime)
+		fmt.Println(elapsed)
 	}
 }
